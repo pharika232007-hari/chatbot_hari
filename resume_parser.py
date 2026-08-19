76d7f1e100ba081e2c5f7386c058ff0c2ba0ec52
pharika232007-hari <pharika23.2007@gmail.com>
2026-08-19 14:57:30 +0530

    Remove resume parser chatbot file

--- resume_parser.py
+++
@@ -1,7 +0,0 @@
-from pdfminer.high_level import extract_text
-def parse_resume(file_path):
-    try:
-        text = extract_text(file_path)
-        return text.lower()
-    except:
-        return ""
